cb6726059a6b41c3f099b79b731c3c8111a526ed
Rivka Revivo <rivkarevivo1997@gmail.com>
2019-04-09 20:42:21 +0300

    Update PhysicalNumber.h

--- PhysicalNumber.h
+++ PhysicalNumber.h
@@ -10,16 +10,16 @@ namespace ariel{
             Unit typeID;
             double units;
         public:
-            PhysicalNumber(double units, Unit type); //constructor
-            Unit getTypeID(); //getter
-            double getUnits(); //getter
+            PhysicalNumber(double units, Unit type); 
+            Unit getTypeID(); 
+            double getUnits(); 
             void setUnits(double units);
-            PhysicalNumber operator+(const PhysicalNumber& other); //diff-types adding
-            PhysicalNumber& operator+=(const PhysicalNumber& other); //adding
-            PhysicalNumber& operator+ (); //unary plus.
-            PhysicalNumber operator-(const PhysicalNumber& other); //diff-types minus
-            PhysicalNumber& operator-=(const PhysicalNumber& other); //minus.
-            PhysicalNumber operator- (); //unary minus.
+            PhysicalNumber operator+(const PhysicalNumber& other); 
+            PhysicalNumber& operator+=(const PhysicalNumber& other); 
+            PhysicalNumber& operator+ (); 
+            PhysicalNumber operator-(const PhysicalNumber& other); 
+            PhysicalNumber& operator-=(const PhysicalNumber& other); 
+            PhysicalNumber operator- (); 
             PhysicalNumber& operator++ ();
             PhysicalNumber& operator-- ();
             PhysicalNumber operator++ (int);
@@ -33,6 +33,6 @@ namespace ariel{
             friend std::ostream& operator<<(std::ostream& os, PhysicalNumber pn);
             friend std::istream& operator>>(std::istream& is, PhysicalNumber pn);
         private:
-            double translateOther(const PhysicalNumber& other); //returns the other.units with type of This.type
+            double translateOther(const PhysicalNumber& other); 
     };
 };
